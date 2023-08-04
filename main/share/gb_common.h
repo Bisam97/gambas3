f937ee9fe015dc1030a7e22114629e8f79456a92
Benoît Minisini <benoit.minisini@gambas-basic.org>
2023-08-04 15:55:39 +0200

    Support for e2k architecture (continue...)
    [INTERPRETER]
    * NEW: Support for e2k architecture (continue...)

--- main/share/gb_common.h
+++ main/share/gb_common.h
@@ -116,14 +116,6 @@
 
 #endif
 
-#ifdef ARCH_E2K // According to AltLinux Sisyphus RPM packages patchs
-
-#ifdef __cplusplus
-#include <bits/locale_facets.h>
-#endif
-
-#endif
-
 #ifndef FALSE
 	enum
 	{
