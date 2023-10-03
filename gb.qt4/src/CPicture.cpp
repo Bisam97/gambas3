62764b8672c65d2740ebbb06999a20e32d5f630a
Benoît Minisini <benoit.minisini@gambas-basic.org>
2023-10-03 08:06:12 +0200

    Still trying to fix 'gb.qt4' compilation.
    [GB.QT4]
    * BUG: Still trying to fix 'gb.qt4' compilation.

--- gb.qt4/src/CPicture.cpp
+++ gb.qt4/src/CPicture.cpp
@@ -124,7 +124,7 @@ CPICTURE *CPICTURE_grab(QWidget *wid, int screen, int x, int y, int w, int h)
 		
 #ifdef QT5
 		//*pict->pixmap = QGuiApplication::primaryScreen()->grabWindow(QX11Info::appRootWindow(), x, y, w, h);
-		PLATFORM.Desktop.Screenshot(pict->pixmap, x, y, w, h); //, screen);
+		PLATFORM.Desktop.Screenshot(pict->pixmap, x, y, w, h, screen);
 #else
 		*pict->pixmap = QPixmap::grabWindow(QX11Info::appRootWindow(), x, y, w, h);
 #endif
